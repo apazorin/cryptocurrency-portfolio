23dd7d6621a7ef6d5d82039edf8562ad36a73163
apazorin <apazorin@devcenter.es>
2022-05-23 13:31:08 +0200

    Wallet - child routes done

--- cryptocurrency-portfolio/src/app/app-routing.module.ts
+++ cryptocurrency-portfolio/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path:'coins', loadChildren: () => import('./coins/coins.module').then(m => m.CoinsModule)},
+  { path:'wallet', loadChildren: () => import('./wallet/wallet.module').then(m => m.WalletModule)},
   { path: '**', redirectTo: 'coins'}
 ];
 
