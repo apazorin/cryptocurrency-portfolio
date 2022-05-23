39a67ceab14d3d27632ca72e1d76ad21f58e371d
apazorin <apazorin@devcenter.es>
2022-05-23 13:34:29 +0200

    Wallet - child routes error solved

--- cryptocurrency-portfolio/src/app/app.module.ts
+++ cryptocurrency-portfolio/src/app/app.module.ts
@@ -3,19 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ListComponent } from './coins/pages/list/list.component';
 import { HttpClientModule } from '@angular/common/http';
 import { SidenavComponent } from './shared/sidenav/sidenav.component';
-import { ListComponent } from './wallet/pages/list/list.component';
-import { WalletComponent } from './wallet/pages/wallet/wallet.component'
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    SidenavComponent,
-    ListComponent,
-    WalletComponent
+    SidenavComponent
   ],
   imports: [
     BrowserModule,
